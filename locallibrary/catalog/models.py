bc4586a60188bea5dc794e4f15bb173f3a13c222
Axel Reyes <fabrayan.r@gmail.com>
2023-06-24 00:38:58 +0000

    5) Add: first model

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+# Genre model
+class Genre(models.Model):
+    """Model representing a book genre."""
+    # Field = column -- my_field_name -- strings of alpahanumeric characters
+    name = models.CharField(max_length=200, help_text='Enter a book genre (e.g. Science Fiction)')
+    # Standar method
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.name
+
