6bf8a9c39a04ba4c1e39f8a1cdd20935f1d61c7a
Axel Reyes <fabrayan.r@gmail.com>
2023-06-24 00:08:26 +0000

    4) Running first migration

--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -17,11 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 from django.views.generic import RedirectView
-from django.conf.import settigs
+from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('catalog/', include('catalog.urls')), #relative url of catalog/urls.py
-    path('', RedirectView.as_view(url='/catalog', permanent = True)), #redirection of root IP
-]+ static (settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+    path('', RedirectView.as_view(url='/catalog/', permanent = True)), #redirection of root IP
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
