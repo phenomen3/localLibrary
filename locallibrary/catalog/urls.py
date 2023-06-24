731fa3c501ac435625838b2cc3f844050791f135
Axel Reyes <fabrayan.r@gmail.com>
2023-06-24 00:01:58 +0000

    3) Add: catalog/urls.py

---
+++ locallibrary/catalog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import include
+#from django.urls import re_path
+from django.urls import repath as url
+from django.conf.urls import url
+
+from . import views
+
+
+urlpatterns = [
+]
