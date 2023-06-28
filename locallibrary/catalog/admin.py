4d29969a40dcaef48c77470296e8c60d0d676c7d
Axel Reyes <fabrayan.r@gmail.com>
2023-06-28 23:11:15 +0000

    16) Modify: adding challenges of part 4

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -13,11 +13,15 @@ admin.site.register(Language)
 
 #Define the admin class
 
+class BooksInline(admin.TabularInline):
+    model = Book
+
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     list_display=('last_name','first_name','date_of_birth','date_of_death')
     #lay out
     fields=['first_name', 'last_name', ('date_of_birth','date_of_death')]
+    inlines = [BooksInline]
 
 class BookInstanceInline(admin.TabularInline):
     model=BookInstance
@@ -31,6 +35,7 @@ admin.site.register(Book,BookAdmin)
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
     list_filter=('status','due_back')
+    list_display=('book','status','due_back','id')
     #Sectioning the detail view
     fieldsets = (
         (None, {
