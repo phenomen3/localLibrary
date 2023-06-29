d8e316cbdb83599be571b869b81c33a0e84412aa
Axel Reyes <fabrayan.r@gmail.com>
2023-06-29 23:19:01 +0000

    Modify: overriding methods in class-based views

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -30,6 +30,11 @@ def index(request):
 
 class BookListView(generic.ListView):
     model = Book
-    context_object_name = 'book_list'   # your own name for the list as a template variable
-    queryset = Book.objects.filter(title__icontains='metamorfosis')[:5] # Get 5 books containing the title metamorfosis
-    template_name = 'books/my_arbitrary_template_name_list.html'  # Specify your own template name/location
+    def get_queryset(self):
+        return Book.objects.filter(title__icontains='war')[:5] # Get 5 books containing the title war
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get the context
+        context = super(BookListView, self).get_context_data(**kwargs)
+        # Create any data and add it to the context
+        context['some_data'] = 'This is just some data'
+        return context
