c5e04e2ec6f7ab2715e94eb1715a11d71ee64c13
phenomen3 <fabrayan.r@gmail.com>
2023-08-12 00:14:18 +0000

    41)ADD:deploying configuration

---
+++ catalog/urls.py
@@ -0,0 +1,11 @@
+#from django.urls import include
+from django.urls import path
+#from django.urls import re_path as url
+#from django.conf.urls import url
+
+from . import views
+
+
+urlpatterns = [
+
+]
